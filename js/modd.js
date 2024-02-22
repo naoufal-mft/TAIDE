ed40a76b3ecde20d8e4617082acb92cedc407ebc
S3kk0um1 <100781739+S3kk0um1@users.noreply.github.com>
2024-02-22 12:10:03 +0100

    Update modd.js

--- js/modd.js
+++ js/modd.js
@@ -13,15 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection= mysql.createConnection({
     host:"localhost",
-    user:"root",
-    password:"azerty",
-    database:"ai_website_db"
+    user:"yourusername",
+    password:"yourpassword",
+    database:"databasename"
 });
 const sessionStore = new MySQLStore({
-    host: "localhost",
-    user: "root",
-    password: "azerty",
-    database: "ai_website_db"
+  host:"localhost",
+    user:"yourusername",
+    password:"yourpassword",
+    database:"databasename",
+    clearExpired: true,
 });
 app.use(session({
     secret: 'dfr324567u6uhbfgfgh8iijmn',
